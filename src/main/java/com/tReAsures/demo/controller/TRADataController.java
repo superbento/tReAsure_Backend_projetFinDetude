fe587f868aac31377a75e5e9c1d0e6c3af5239eb
superbento <37655893+superbento@users.noreply.github.com>
2019-10-22 17:16:48 +0200

    initializer

---
+++ src/main/java/com/tReAsures/demo/controller/TRADataController.java
@@ -0,0 +1,18 @@
+package com.tReAsures.demo.controller;
+
+import io.swagger.annotations.ApiOperation;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/***
+ * 
+ * @author Shuo LIANG
+ *
+ */
+public class TRADataController {
+
+}
